bce3d3843e6becbf9ccad9c0cf2a10291cd4f70f
Adam Cimarosti <cimarosti@gmail.com>
2022-07-14 16:15:07 +0100

    Bump version: 0.0.1 → 0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'questdb'
 year = '2022'
 author = 'QuestDB'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.1'
+version = release = '0.0.2'
 
 github_repo_url = 'https://github.com/questdb/py-questdb-client'
 
