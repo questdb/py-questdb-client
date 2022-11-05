7b5200ecbd13a4d2fad5d4f1f65fa07818f81134
Adam Cimarosti <cimarosti@gmail.com>
2022-11-05 11:54:55 +0000

    Reworked API for better buffer reuse and for individual UCS1, 2, 4 functions.

--- pystr-to-utf8/src/lib.rs
+++ pystr-to-utf8/src/lib.rs
@@ -1,31 +1,78 @@
-use std::ffi::{c_void, c_char};
+use std::ffi::c_char;
 use std::fmt::Write;
+use std::slice::from_raw_parts;
 
 #[allow(non_camel_case_types)]
-pub struct converter(String);
+pub struct qdb_pystr_buf(String);
 
 #[no_mangle]
-pub unsafe extern "C" fn questdb_pystr_converter_new() -> *mut converter {
-    Box::into_raw(Box::new(converter(String::with_capacity(64))))
+pub unsafe extern "C" fn qdb_pystr_buf_new() -> *mut qdb_pystr_buf {
+    Box::into_raw(Box::new(qdb_pystr_buf(String::with_capacity(64))))
+}
+
+/// Get current position. Use in conjunction with `truncate`.
+#[no_mangle]
+pub unsafe extern "C" fn qdb_pystr_buf_tell(b: *mut qdb_pystr_buf) -> usize {
+    let b = &mut *b;
+    b.0.len()
+}
+
+/// Trim the string to the given length. Use in conjunction with `tell`.
+#[no_mangle]
+pub unsafe extern "C" fn qdb_pystr_buf_truncate(
+        b: *mut qdb_pystr_buf, len: usize) {
+    let b = &mut *b;
+    b.0.truncate(len)
+}
+
+/// Reset the converter's buffer to zero length.
+#[no_mangle]
+pub unsafe extern "C" fn qdb_pystr_buf_clear(b: *mut qdb_pystr_buf) {
+    let b = &mut *b;
+    b.0.clear()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn questdb_pystr_converter_free(c: *mut converter) {
-    if !c.is_null() {
-        drop(Box::from_raw(c));
+pub unsafe extern "C" fn qdb_pystr_buf_free(b: *mut qdb_pystr_buf) {
+    if !b.is_null() {
+        drop(Box::from_raw(b));
     }
 }
 
-fn encode_ucs1(dest: &mut String, buf: &[u8]) -> bool {
+#[inline]
+fn encode_ucs1(dest: &mut String, buf: &[u8]) {
     // len(chr(2 ** 8 - 1).encode('utf-8')) == 2
     let utf8_mult = 2;
     dest.reserve(utf8_mult * buf.len());
-    for b in buf.iter() {
-        dest.push(*b as char);
+    for &b in buf.iter() {
+        dest.push(b as char);
     }
-    true
 }
 
+#[no_mangle]
+pub unsafe extern "C" fn qdb_ucs1_to_utf8(
+        b: *mut qdb_pystr_buf,
+        count: usize, input: *const u8,
+        size_out: *mut usize, buf_out: *mut *const c_char) {
+    let b = &mut *b;
+    let i = from_raw_parts(input, count);
+    if i.is_ascii() {
+        // Zero-copy optmization:
+        // is_ascii does a WORD-sized scan, so it's faster than the copy logic.
+        // We can also avoid the copy altogether and return back the same ptr.
+        // We get away with this because 7-bit ascii is a subset of UTF-8.
+        *size_out = count;
+        *buf_out = input as *const c_char;
+    }
+    else {
+        let last = b.0.len();
+        encode_ucs1(&mut b.0, i);
+        *size_out = b.0.len() - last;
+        *buf_out = b.0.as_ptr() as *const c_char;
+    }
+}
+
+#[inline]
 fn encode_ucs2(dest: &mut String, buf: &[u16]) -> bool {
     // len(chr(2 ** 16 - 1).encode('utf-8')) == 3
     let utf8_mult = 3;
@@ -48,6 +95,20 @@ fn encode_ucs2(dest: &mut String, buf: &[u16]) -> bool {
     true
 }
 
+#[no_mangle]
+pub unsafe extern "C" fn qdb_ucs2_to_utf8(b: *mut qdb_pystr_buf,
+        count: usize, input: *const u16,
+        size_out: *mut usize, buf_out: *mut *const c_char) -> bool {
+    let b = &mut *b;
+    let i = from_raw_parts(input, count);
+    let last = b.0.len();
+    let ok = encode_ucs2(&mut b.0, i);
+    *size_out = b.0.len() - last;
+    *buf_out = b.0.as_ptr() as *const c_char;
+    ok
+}
+
+#[inline]
 fn encode_ucs4(dest: &mut String, buf: &[u32]) -> bool {
     // Max 4 bytes allowed by RFC: https://www.rfc-editor.org/rfc/rfc3629#page-4
     let utf8_mult = 4;
@@ -65,37 +126,15 @@ fn encode_ucs4(dest: &mut String, buf: &[u32]) -> bool {
     true
 }
 
-/// Converts a Python string to a UTF8 buffer.
-/// * Width is 1 for UCS1, 2 for UCS2, 4 for UCS4.
-/// * Count is the number of code points.
-/// * Input is the pointer to the UCS{1,2,4} data.
-/// * size_out is the resulting size in bytes of the UTF8 string.
-/// * buf_out is set to point to the UTF8 string.
-/// Returns true for success of false for failure.
-/// In case of failure, size_out and buf_out contain the error message.
 #[no_mangle]
-pub unsafe extern "C" fn questdb_pystr_to_convert(
-        c: *mut converter,
-        width: u8, count: usize, input: *const c_void,
+pub unsafe extern "C" fn qdb_ucs4_to_utf8(b: *mut qdb_pystr_buf,
+        count: usize, input: *const u32,
         size_out: *mut usize, buf_out: *mut *const c_char) -> bool {
-    let sbuf: &mut String = &mut (*c).0;
-    sbuf.clear();
-    let ok = match width {
-        1 => encode_ucs1(
-            sbuf,
-            std::slice::from_raw_parts(input as *const u8, count)),
-        2 => encode_ucs2(
-            sbuf,
-            std::slice::from_raw_parts(input as *const u16, count)),
-        4 => encode_ucs4(
-            sbuf,
-            std::slice::from_raw_parts(input as *const u32, count)),
-        _ => {
-            write!(sbuf, "Unsupported width: {}", width).unwrap();
-            false
-        },
-    };
-    *size_out = sbuf.len();
-    *buf_out = sbuf.as_ptr() as *const c_char;
+    let b = &mut *b;
+    let i = from_raw_parts(input, count);
+    let last = b.0.len();
+    let ok = encode_ucs4(&mut b.0, i);
+    *size_out = b.0.len() - last;
+    *buf_out = b.0.as_ptr() as *const c_char;
     ok
 }
