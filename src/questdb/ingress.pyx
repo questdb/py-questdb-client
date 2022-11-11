ef6735bee0cf7dc9372580ffef2b0b10d57e10a0
Adam Cimarosti <cimarosti@gmail.com>
2022-11-11 10:47:38 +0000

    Added arrow C data interface type definitions.

--- src/questdb/ingress.pyx
+++ src/questdb/ingress.pyx
@@ -46,46 +46,8 @@ from cpython.memoryview cimport PyMemoryView_FromMemory
 
 from .line_sender cimport *
 from .pystr_to_utf8 cimport *
-
-cdef extern from "Python.h":
-    ctypedef uint8_t Py_UCS1  # unicodeobject.h
-    ctypedef uint16_t Py_UCS2
-    ctypedef uint32_t Py_UCS4
-
-    ctypedef unsigned int uint
-
-    cdef enum PyUnicode_Kind:
-        PyUnicode_1BYTE_KIND
-        PyUnicode_2BYTE_KIND
-        PyUnicode_4BYTE_KIND
-
-    # Note: Returning an `object` rather than `PyObject` as the function
-    # returns a new reference rather than borrowing an existing one.
-    object PyUnicode_FromKindAndData(
-        int kind, const void* buffer, Py_ssize_t size)
-
-    # Ditto, see comment on why not returning a `PyObject` above.
-    str PyUnicode_FromStringAndSize(
-        const char* u, Py_ssize_t size)
-
-    # Must be called before accessing data or is compact check.
-    int PyUnicode_READY(object o) except -1
-
-    # Is UCS1 and ascii (and therefore valid UTF-8).
-    bint PyUnicode_IS_COMPACT_ASCII(object o)
-
-    # Get length.
-    Py_ssize_t PyUnicode_GET_LENGTH(object o)
-
-    # Zero-copy access to string buffer.
-    int PyUnicode_KIND(object o)
-    Py_UCS1* PyUnicode_1BYTE_DATA(object o)
-    Py_UCS2* PyUnicode_2BYTE_DATA(object o)
-    Py_UCS4* PyUnicode_4BYTE_DATA(object o)
-
-    Py_ssize_t PyBytes_GET_SIZE(object o)
-
-    char* PyBytes_AsString(object o)
+from .arrow_c_data_interface cimport *
+from .extra_cpython cimport *
 
 
 import cython
