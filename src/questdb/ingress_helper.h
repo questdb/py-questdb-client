9b75a12935974124f8aad0f33bbb8cd60dae8e89
Adam Cimarosti <cimarosti@gmail.com>
2022-12-01 18:13:30 +0000

    Formatting.

--- src/questdb/ingress_helper.h
+++ src/questdb/ingress_helper.h
@@ -6,6 +6,8 @@
 // If we do this in Cython it treats `buf.obj` as
 // a ref-counted `object` instead of a `PyObject*`,
 // so we can't check it for NULL.
+// Since `Py_buffer` is a Cython built-in we can't actually
+// just re-define it in `extra_cpython.pxd`.
 inline int Py_buffer_obj_is_set(Py_buffer* buf)
 {
     return buf->obj != NULL;
