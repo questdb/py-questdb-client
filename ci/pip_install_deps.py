93cb605545472a6d1570bf2ccbb37d664837cf66
Adam Cimarosti <cimarosti@gmail.com>
2023-11-06 18:13:23 +0000

    CI dependency fix

--- ci/pip_install_deps.py
+++ ci/pip_install_deps.py
@@ -54,6 +54,7 @@ def ensure_timezone():
 
 def main():
     ensure_timezone()
+    try_pip_install('setuptools')
     try_pip_install('fastparquet>=2022.12.0')
     try_pip_install('pandas')
     try_pip_install('numpy')
