a885182aeec093e9486480029307112e07acb1e5
javier <javier@formatinternet.com>
2024-07-19 17:32:37 +0200

    changing examples to the trades dataset

--- examples/pandas_basic.py
+++ examples/pandas_basic.py
@@ -6,19 +6,17 @@ import pandas as pd
 
 def example(host: str = 'localhost', port: int = 9000):
     df = pd.DataFrame({
-            'pair': ['USDGBP', 'EURJPY'],
-            'traded_price': [0.83, 142.62],
-            'qty': [100, 400],
-            'limit_price': [0.84, None],
-            'timestamp': [
-                pd.Timestamp('2022-08-06 07:35:23.189062', tz='UTC'),
-                pd.Timestamp('2022-08-06 07:35:23.189062', tz='UTC')]})
+            'symbol': pd.Categorical(['ETH-USD', 'BTC-USD']),
+            'side': pd.Categorical(['sell', 'sell']),
+            'price': [2615.54, 39269.98],
+            'amount': [0.00044, 0.001],
+            'timestamp': pd.to_datetime(['2021-01-01', '2021-01-02'])})
     try:
         with Sender.from_conf(f"http::addr={host}:{port};") as sender:
             sender.dataframe(
                 df,
                 table_name='trades',  # Table name to insert into.
-                symbols=['pair'],  # Columns to be inserted as SYMBOL types.
+                symbols=['symbol', 'side'],  # Columns to be inserted as SYMBOL types.
                 at='timestamp')  # Column containing the designated timestamps.
 
     except IngressError as e:
