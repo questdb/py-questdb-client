a1ad8ab13571c9ed466bbc8482c66fac5f3e8f03
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2024-08-13 14:51:56 +0100

    Update version and check if rebuild is inconsistent

--- ci/pip_install_deps.py
+++ ci/pip_install_deps.py
@@ -4,8 +4,6 @@ import shlex
 import textwrap
 import platform
 import argparse
-import os
-
 
 arg_parser = argparse.ArgumentParser(
     prog='pip_install_deps.py',
@@ -13,6 +11,7 @@ arg_parser = argparse.ArgumentParser(
 )
 
 arg_parser.add_argument('--pandas-version')
+
 
 class UnsupportedDependency(Exception):
     pass
@@ -36,8 +35,8 @@ def pip_install(package, version=None):
         return
     output = res.stdout.decode('utf-8')
     is_unsupported = (
-        ('Could not find a version that satisfies the requirement' in output) or
-        ('The conflict is caused by' in output))
+            ('Could not find a version that satisfies the requirement' in output) or
+            ('The conflict is caused by' in output))
     if is_unsupported:
         raise UnsupportedDependency(output)
     else:
@@ -82,14 +81,14 @@ def main(args):
         try_pip_install('pyarrow')
 
     on_linux_is_glibc = (
-        (not platform.system() == 'Linux') or
-        (platform.libc_ver()[0] == 'glibc'))
-    is_64bits = sys.maxsize > 2**32
+            (not platform.system() == 'Linux') or
+            (platform.libc_ver()[0] == 'glibc'))
+    is_64bits = sys.maxsize > 2 ** 32
     is_cpython = platform.python_implementation() == 'CPython'
     is_windows_py3_12 = (
         # https://github.com/dask/fastparquet/issues/892
-        platform.system() == 'Windows' and
-        sys.version_info >= (3, 12))
+            platform.system() == 'Windows' and
+            sys.version_info >= (3, 12))
     if on_linux_is_glibc and is_64bits and is_cpython:
         # Ensure that we've managed to install the expected dependencies.
         import pandas
